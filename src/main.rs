63142120f2e0eedabe40d131c548a4b9f177c4ef
Anton Dort-Golts <dortgolts@gmail.com>
2023-07-09 23:37:29 +0100

    [WIP] simplification

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-// TODO remove after test/debug
-// use pest::Parser;
-// use pest_derive::Parser;
-
 mod eval;
 mod parser;
+mod simp;
 
 use parser::parse;
 
@@ -12,7 +9,6 @@ use std::process::exit;
 fn main() {
     let test_str: &str =
         "!!c \n | a_1  *! ( \n c  &! _d12) | [(12a & !__12)  ] + !( !a_2 *!cdeFG_12_op)";
-    // let test_str: &str = "\n";
 
     match parse(test_str) {
         Ok(ast) => {
@@ -23,17 +19,7 @@ fn main() {
             exit(1);
         }
     }
-
-    // let test_str = " a_1 *! ( c*!_d12) + !( !a_2 *!cdeFG_12_op)";
-
-    // let pairs = LogicParser::parse(Rule::expr, test_str).expect("WTF?");
-    // for pair in pairs.into_iter() {
-    //     println!("{:#?}", pair)
-    // }
 }
 
 // TODO impl:
-//  1. Expression tokenizer
-//  2. Expression parser
-//  3. AST builder (optional compilation ?)
-//  4. Quine-McCluskey logic simplifier
+//  - Quine-McCluskey logic simplifier
