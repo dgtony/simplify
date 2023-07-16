2908015ada0d1aa1781dcb625532e968d08e53a5
Anton Dort-Golts <dortgolts@gmail.com>
2023-07-16 15:29:03 +0100

    simplify expression from the first CLI arg

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,39 @@ mod eval;
 mod parser;
 mod simp;
 
-use parser::parse;
-
+use std::env;
 use std::process::exit;
 
-fn main() {
-    let test_str: &str =
-        "!!c \n | a_1  *! ( \n c  &! _d12) | [(12a & !__12)  ] + !( !a_2 *!cdeFG_12_op)";
+use parser::parse;
+use simp::simplify;
 
-    match parse(test_str) {
-        Ok(ast) => {
-            println!("[OK] expression AST:\n{:#?}", ast);
+fn main() {
+    let expr = match env::args().skip(1).next() {
+        Some(arg) => arg,
+        None => {
+            println!("provide boolean expression to simplify");
+            exit(0);
         }
+    };
+
+    let ast = match parse(&expr) {
+        Ok(parsed) => parsed,
         Err(e) => {
-            println!("[ERR] parsing expression: {}", e);
+            println!("[ERR] expression parsing: {}", e);
             exit(1);
         }
+    };
+
+    let solutions = match simplify(&ast) {
+        Some(ss) => ss,
+        None => {
+            println!("[ERR] cannot simplify: too many variables");
+            exit(1);
+        }
+    };
+
+    println!("[OK] {} solution(s) found", solutions.len());
+    for solution in solutions {
+        println!("=> {}", solution);
     }
 }
-
-// TODO impl:
-//  - Quine-McCluskey logic simplifier
